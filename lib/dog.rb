08bc24c071582d9a91078b0a2e57dc4c69b92c92
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-15 02:34:22 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,44 @@
+class Dog
+ 
+  def name=(new_name)
+    @name = new_name
+  end
+ 
+  def name
+    @name
+  end
+  def breed=(breed)
+    @breed = breed
+  end
+ def breed
+   @breed
+ end
+  
+end
+
+
+
+
+
+
+
+
+
+# class Dog
+ 
+#   def initialize(first_name, last_name)
+#     @first_name = first_name
+#     @last_name = last_name
+#   end
+ 
+#   def name=(full_name)
+#     first_name, last_name = full_name.split
+#     @first_name = first_name
+#     @last_name = last_name
+#   end
+ 
+#   def name
+#     "#{@first_name} #{@last_name}".strip
+#   end
+ 
+# end
