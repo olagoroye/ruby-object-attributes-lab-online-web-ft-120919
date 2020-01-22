11a911d7725a6f394d09a822ce6925474493fe4c
olusola olagoroye <solaolagoroye@gmail.com>
2020-01-22 22:25:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,18 @@
 class Dog
  
+<<<<<<< HEAD
   def name=(new_name)
     @name = new_name
+=======
+  def initialize(name)
+    @name = name
+>>>>>>> d1903ab46dc2aeb677f7b273e07411e6cd5e440b
   end
  
   def name
     @name
   end
+<<<<<<< HEAD
   def breed=(breed)
     @breed = breed
   end
@@ -14,6 +20,13 @@ class Dog
    @breed
  end
   
+=======
+ 
+  def name=(new_name)
+    @name = new_name
+  end
+ 
+>>>>>>> d1903ab46dc2aeb677f7b273e07411e6cd5e440b
 end
 
 
